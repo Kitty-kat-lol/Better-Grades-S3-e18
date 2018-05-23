24446c5beb6da2b945952e1a943e4a5923c61954
Kitty-kat-lol <32311727+Kitty-kat-lol@users.noreply.github.com>
2018-05-23 10:19:47 -0400

    c la faute de gab

--- Graphic.js
+++ Graphic.js
@@ -1,1 +1,31 @@
 //The begenning graphique
+
+<html>
+<body>
+
+<h1>My First JavaScript</h1>
+
+<p>JavaScript can change the content of an HTML element:</p>
+
+<button type="button" onclick="myFunction1()">Click Me!</button>
+
+<p id="demo">Ca marche pas.</p>
+
+<script>
+function myFunction1() {
+    document.getElementById("demo").innerHTML = "C'est la faute de Gab!";
+}
+</script>
+
+<button type="button" onclick="myFunction()">Click Me!</button>
+
+<p id="demo1">Ca marche toujour pas ?</p>
+
+<script>
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "C'est encore la faute de Gab!";
+}
+</script>
+
+</body>
+</html>
