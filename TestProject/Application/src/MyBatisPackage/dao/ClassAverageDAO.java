47296fab02d11385c20a07ae5062f743cf66dd01
WhatAProphet <gabriel_lessard@hotmail.com>
2018-06-11 13:26:51 -0400

    Mybatis 2.0

---
+++ TestProject/Application/src/MyBatisPackage/dao/ClassAverageDAO.java
@@ -0,0 +1,36 @@
+package MyBatisPackage.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import MyBatisPackage.mapper.ClassAverageMapper;
+import MyBatisPackage.model.ClassAverage;
+import MyBatisPackage.util.MyBatisDAOUtil;
+
+public class ClassAverageDAO {
+	
+	public List<ClassAverage> getAllClassAverageByCIP(String cip) {
+		SqlSession sqlSession = MyBatisDAOUtil.getSqlSessionFactory()
+				.openSession();
+		try {
+			ClassAverageMapper classAverageMapper = sqlSession.getMapper(ClassAverageMapper.class);
+			return classAverageMapper.getAllClassAverageByCIP(cip);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	/*public ClassAverage getClassAverageByCIPAndCoursId(String cip, String coursId) {
+		
+		SqlSession sqlSession = MyBatisDAOUtil.getSqlSessionFactory()
+				.openSession();
+		try {
+			ClassAverageMapper classAverageMapper = sqlSession.getMapper(ClassAverageMapper.class);
+			return classAverageMapper.getClassAverageByCIPAndCoursId(cip, coursId);
+		} finally {
+			sqlSession.close();
+		}
+		
+	}*/
+}
