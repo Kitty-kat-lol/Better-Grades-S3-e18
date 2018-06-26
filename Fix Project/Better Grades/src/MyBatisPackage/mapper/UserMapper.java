7e430ec2575db3dca94e6761ad723466bc72b447
WhatAProphet <gabriel_lessard@hotmail.com>
2018-06-26 14:03:00 -0400

    merge

--- Fix Project/Better Grades/src/MyBatisPackage/mapper/UserMapper.java
+++ Fix Project/Better Grades/src/MyBatisPackage/mapper/UserMapper.java
@@ -1,25 +1,2 @@
 
 package MyBatisPackage.mapper;
-
-import java.util.List;
-
-import MyBatisPackage.model.User;
-
-/**
- * 
- * @author karthy
- *
- */
-public interface UserMapper {
-
-	public void insertUser(User user);
-
-	public User getUserById(Integer userId);
-
-	public List<User> getAllUsers();
-
-	public void updateUser(User user);
-
-	public void deleteUser(Integer userId);
-
-}
